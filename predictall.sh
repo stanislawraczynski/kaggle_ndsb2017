8e13095ef1242dd72e9baccbb123a896d396b876
Stanisław Raczyński <stanislaw.raczynski@pictec.eu>
2018-03-21 08:40:58 +0100

    Fix

--- predictall.sh
+++ predictall.sh
@@ -2,5 +2,5 @@
 filelist=`ls data/gumed/*.mhd | sed -e 's/data\/gumed\///g' -e 's/\.mhd//g'`
 for filename in $filelist; do
     echo "Running for $filename"
-    python gumed_predict.py $filename
+    python predict_raw.py $filename
 done
